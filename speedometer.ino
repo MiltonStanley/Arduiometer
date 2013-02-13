56e51ababe8d81b72fa71d80ceecde69e77c8227
MiltonStanley <milton.stanley@gmail.com>
2013-02-13 11:04:43 -0600

    Personalized code & constants

--- speedometer.ino
+++ speedometer.ino
@@ -3,17 +3,19 @@
 // theoretically reads the number of times a wheel rotates per second,
 // and displays that number converted to miles per hour based on wheel radius
 
-//pin stuffs
-int inputPin=8;       // pin to read hits
+// Constants
+int SWITCH=8;       // pin to read hits
+double TIRERADIUS=12.5;      //tire radius in inches (12.5 inches in my case)
+double PI = 3.14159265358979323846264338327950;
+double TIRECURCUMFERENCE= 2 * PI * TIRERADIUS; //calculates tire circumference
+
+
+//mathing
 double lastHit=0;        // stores last hit (ms)
 double thisHit=0;        //this hit (ms)
 int pinState=0;       //reads input pin
 int totalHits=0;
-
-//mathing
 double mph=0;                   // Miles Per Hours; displayed
-double tireRadius=12.5;      //tire radius in inches (12.5 inches in my case)
-double tireCircumference= (double) 2.0 * 3.14159265358979323846264338327950 * tireRadius; //calculates tire circumference
 double hps=0;
 double inches=0;
 
@@ -24,13 +26,13 @@ long interval = 999;        // not quite a second, to compensate for LED blink
 
 void setup()
 {
-  pinMode(inputPin, INPUT);
+  pinMode(SWITCH, INPUT);
   Serial.begin(9600);
 }
 
 void loop()
 {
-  pinState=digitalRead(inputPin);
+  pinState=digitalRead(SWITCH);
   
   if(pinState==HIGH)
   {
@@ -50,7 +52,7 @@ int convert(double hitMillis)      // converts millisecond since last hit to mph
   
   hps = (double) 1000.0 / hitMillis;
 
-  inches = (double) hps * tireCircumference;
+  inches = (double) hps * TIRECURCUMFERENCE;
   
   mph = (double) inches * (5.0/88.0); //convert hits per second to miles per hour
   Serial.println(mph);
